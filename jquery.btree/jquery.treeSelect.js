ca6c3900994038af7305360659d2388d34172eb2
yuanxin.qiu <yuanxin.qiu@easyread.ly>
2014-06-29 23:37:01 +0800

    reach milestone

--- jquery.btree/jquery.treeSelect.js
+++ jquery.btree/jquery.treeSelect.js
@@ -1,45 +1,271 @@
 (function($) {
 
-    // tree.hide
-    // tree.show
-    // tree.getSelected
+    var HELPER = {
+        CovertNodesToHTML: function (nodes, level, display) {
+            var html = ''
+              , basePL    = 15
+              , currentPL = 0
+              , ulCss     = ""
+
+            if (level == 0) { // Root Node
+                ulCss += "ui-simpletree simpletree-container";
+            }
+
+            var ulStyle = level == 0 || display ? "" : " style='display:none' ";
+
+            // 计算该 ul 下的结点应该缩进缩少 px;
+            currentPL = level * basePL;
+
+            html += '<ul tabindex="0" class="' + ulCss + '" ' + ulStyle + '" _pl="' + currentPL + '">';
+
+            for (var i = 0; i < nodes.length; i++) {
+                var n = nodes[i];
+
+                var spanCss = HELPER.GetSpanCss(n);
+
+                html += '<li>';
+                html += '<div _pid="' + n.pid + '" class="' + spanCss + ' " style="padding-left:' + currentPL +'px;">';
+                html += '<span class="simpletree-expander"></span>';
+
+                html += HELPER.GetIconHTML(n);
+                html += HELPER.GetTitleHTML(n);
+
+                html += '</div>';
+
+                if (n.children && n.children.length > 0) {
+                    html += HELPER.CovertNodesToHTML(n.children, level + 1, n.isExpanded);
+                }
+
+                html += '</li>';
+            }
+
+            html += '</ul>';
+            return html;
+        },
+
+        GetSpanCss: function(node) {
+            var css = "simpletree-node "
+
+            if (node.liClass) {
+                css += node.liClass
+            }
+
+            if (node.isActive) {
+                css += " simpletree-node-active"
+            }
+
+            if (node.isExpanded) {
+                css += " simpletree-node-on";
+            }
+
+            css += (node.isExpanded ? " simpletree-exp-e" : " simpletree-exp-c")
+
+            return css;
+        },
+
+        GetIconHTML: function(node) {
+            if (node.iconUrl) {
+                return '<span class="simpletree-custom-icon"><img src="' + node.iconUrl + '"></span>';
+            } else {
+                return '<span class="simpletree-icon"></span>';
+            }
+        },
+
+        GetTitleHTML: function(node) {
+            var html = '';
+            var tooltip = node.tooltip ? 'title="' + node.tooltip + '"' : "";
+
+            var css = "simpletree-title";
+
+            if (node.textCss) {
+                css += (" " + node.textCss);
+            }
+
+            html += '<span ' + tooltip + ' class="' + css + '">';
+            html += node.text;
+            html += '</span>';
+
+            return html;
+        },
+
+        FindNodeByPid: function(nodes, pid) {
+            for(var i = 0; i < nodes.length; i++) {
+                var node = nodes[i];
+
+                if (node.pid == pid) {
+                    return node;
+                } else if (node.children && node.children.length > 0) {
+                    var node_1 = HELPER.FindNodeByPid(node.children, pid);
+                    if (node_1) {
+                        return node_1;
+                    }
+                }
+            }
+
+            return null;
+        },
+
+        UnselectAll: function(nodes) {
+            for (var i = 0; i < nodes.length; i++) {
+                nodes[i].isActive = false
+                if (nodes[i].children && nodes[i].children.length > 0) {
+                    HELPER.UnselectAll(nodes[i].children);
+                }
+            };
+        }
+    }
 
     $.fn.treeSelect = function(options) {
-
         var $input = this;
 
-        // 默认配置
-        var _settings = {
-
-            // value key of the TREE NODE, default: 'pid'
-            valueKey: 'pid',
-
-            // text of the OK_BUTTON
-            okBtnText: "确定",
-
-            // text of the CALCEL_BUTTON
-            cancelBtnText: "取消",
-
-            // Load tree data from server
-            remote: {
-                enable: false,    // default to disable
-                url: null,
-                type: "GET",
-                dataType: "JSON"
-            },
-
-            // callback after click OK_BUTTON
-            onOkClick: null,
-
-            // callback after click CALCEL_BUTTON
-            onCancelClick: null // Default to hide the panel
+        // Global
+        // To store the data;
+        var DB = {
+            nodes: null,
+            selectedNodeContext: null
         };
 
-        // For rendering the tree view
-        function TreeView() {
+        // For rendering the tree
+        function TreeView(treeViewConf) {
+            var treeObj = this;
+
+            this.$container = null;   // TreeView Container Context
+            this.$el = null;          // TreeView Context
+
+            var _settings = {
+                renderTo: null,         // should be a jqContext
+                dataUrl: null,  // Default to null;
+                slidingTime: 100,       // Expand/Collapse children with 'slide'
+            };
+
+            this.getSelectedNode = function() {
+                if (DB.selectedNodeContext) {
+                    var pid = parseInt(DB.selectedNodeContext.attr("_pid"))
+                    return HELPER.FindNodeByPid(pid);
+                } else {
+                    return null;
+                }
+            }
+
+            this.selectNodeByPid = function(pid) {
+                DB.selectedNodeContext = null;
+
+                this.$el.find(".simpletree-node")
+                    .each(function(el, index){
+                        $node = $(el)
+                        _pid = parseInt($node.attr("_pid"))
+                        if (_pid == pid) {
+                            DB.selectedNodeContext = $node;
+                            $node.addClass("simpletree-node-active");
+                        } else {
+                            $node.removeClass("simpletree-node-active");
+                        }
+                    })
+            }
+
+            this.unselectAll = function() {
+                DB.selectedNodeContext.removeClass("simpletree-node-active");
+                DB.selectedNodeContext = null;
+            }
+
+            function buildTree() {
+                if (treeObj.$el != null) {
+                    treeObj.$el.empty();
+                    treeObj.$el = null;
+                }
+
+                // Covert nodes to HTML
+                treeObj.$el = $(HELPER.CovertNodesToHTML(DB.nodes, 0, true));
+
+                // Append to treeview container
+                treeObj.$container.empty();
+                treeObj.$container.append(treeObj.$el);
+
+                // Bind node events
+                // Toggle the Node
+                treeObj.$el.find(".simpletree-node").on('click', function(e){
+                    var currentPid = null;
+
+                    if (DB.selectedNodeContext) {
+                        currentPid = parseInt(DB.selectedNodeContext.attr("_pid"));
+                    }
+
+                    var $this = $(this)
+                      , pid   = parseInt($this.attr("_pid"));
+
+                    if (currentPid && pid == currentPid) {
+                        if ($this.hasClass("simpletree-exp-c")) {
+                            $this.addClass("simpletree-exp-e");
+                            $this.removeClass("simpletree-exp-c");
+                            $this.siblings("ul").css("display", "block");
+                        } else {
+                            $this.addClass("simpletree-exp-c");
+                            $this.removeClass("simpletree-exp-e");
+                            $this.siblings("ul").css("display", "none");
+                        }
+                    } else {
+                        if (DB.selectedNodeContext) {
+                            DB.selectedNodeContext.removeClass("simpletree-node-active");
+                        }
+                        $this.addClass("simpletree-node-active");
+                        DB.selectedNodeContext = $this;
+                    }
+                });
+            }
+
+            this.render = function() {
+                if (_settings.data) {
+                    DB.nodes = _settings.data;
+                    buildTree();
+                } else if (_settings.dataUrl) {
+                    $.ajax({
+                        url: _settings.dataUrl,
+                        type: "GET",
+                        dataType: "JSON",
+                        success: function(data, textStauts, jqXHR) {
+                            DB.nodes = data;
+                            buildTree();
+                        },
+                        error: function(jqXHR, textStauts, errorThrown) {
+                            console.dir(jqXHR);
+                            alert("加载数据失败");
+                        }
+                    });
+                } else {
+                    console.warn("Tree without data!");
+                }
+            }
+
+            $.extend(true, _settings, treeViewConf);
+
+            if (_settings.renderTo) {
+                treeObj.$container = _settings.renderTo;
+            } else {
+                alert("renderTo can't not be null.");
+            }
+
+            return this;
         }
 
+        // The Tree SelectBox Obj
         function TreeDropdown(configuration) {
+            var _settings = {
+                // text of the OK_BUTTON
+                okBtnText: "确定",
+
+                // text of the CALCEL_BUTTON
+                cancelBtnText: "取消",
+
+                // callback after click OK_BUTTON
+                onOkClick: null,
+
+                // callback after click CALCEL_BUTTON
+                onCancelClick: null,
+                tree: {
+                    data: null,    // Build tree with data
+                    dataUrl: null  // IF data IS null THEN LOAD FROM SERVER
+                }
+            };
 
             /**************************************
             ** Public variables and methods
@@ -92,6 +318,11 @@
 
             // 得到选中的树结点的数据
             this.getSelected = function() {
+                if (this.treeObj) {
+                    return this.treeObj.getSelectedNode();
+                } else {
+                    return null;
+                }
             }
 
             /**************************************
@@ -107,8 +338,6 @@
             var $treeSelectPanel  = null
               , $treeSelectBody   = null
               , $treeSelectFooter = null
-
-            var currentNode = null;
 
             // Private methods
             function mergeConfiguration() {
@@ -167,7 +396,7 @@
 
                 $tsEmpty.on('click', function(e) {
                     $tddInput.val('')
-                    currentNode = null;
+                    treeddControl.treeObj.unselectAll();
                 })
 
                 $tpfCancelBtn.on('click', function(e) {
@@ -178,25 +407,23 @@
                     e.preventDefault();
 
                     if (treeddControl.treeObj) {
-                        // You have to select a node
-                        if (currentNode == null) {
-                            return false;
-                        }
+                        var node = treeddControl.treeObj.getSelectedNode();
 
                         // Hide the TreeDropdwon panel
                         treeddControl.hide();
 
                         // Update display dom nodes
-                        $tsInput.val(currentNode[_settings.valueKey])
-                        $tsText.html(currentNode["text"])
+                        $tsInput.val(node["pid"])
+                        $tsText.html(node["text"])
 
                         // fire the OK_BUTTON click callback
                         if (_settings.callback.onOkClick) {
-                            _settings.callback.onOkClick.apply(treeddControl, { node: currentNode })
+                            _settings.callback.onOkClick.apply(treeddControl, { node: node })
                         }                   
                     } else {
                         // oh my god, it's too slow.
                         console.warn("Tree is not ready yet!");
+                        treeObj.$container.html("No data!");
                         return false;
                     }
                 })
@@ -208,26 +435,20 @@
             }
 
             function buildTree() {
+                var tvConfig = _settings.tree;
+
+                // Init the TreeView Container
+                tvConfig.renderTo = $tpBody;
+
+                treeddControl.treeObj = new TreeView(tvConfig);
+                treeddControl.treeObj.render();
             }
 
-            function init() {
-                // 初始化配置
-                mergeConfiguration()
-
-                // 初始化面板(弹出面板控件，面板，树容器)
-                initTDDContext();
-
-                // 显示控件
-                showTreeddPanel();
-
-                // 初始化事件(弹出按钮点击事件，初始化取消按钮事件)
-                setupEvents();
-
-                // 加载数据，初始化树
-                buildTree();
-            }
-
-            init();
+            mergeConfiguration();   // 初始化配置
+            initTDDContext();       // 初始化面板(弹出面板控件，面板，树容器)
+            showTreeddPanel();      // 显示控件
+            setupEvents();          // 初始化事件(弹出按钮点击事件，初始化取消按钮事件)
+            buildTree();            // 加载数据，初始化树
 
             return this;
         }
